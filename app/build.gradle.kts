7c2ebc84f13171e8b1b14c5168d62795cad810d4
VictorGabriel1 <victor.mucci1@hotmail.com>
2024-03-19 10:57:26 -0300

    config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.servicesclub"
+    namespace = "com.example.health"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.servicesclub"
+        applicationId = "com.example.health"
         minSdk = 31
         targetSdk = 34
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ServicesClub"
+rootProject.name = "Health"
 include(":app")
